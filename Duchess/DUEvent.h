4d2e6a2f75ae1a34de2e72a6471901bb7d646b39
Matthew Bates <matthew117@hotmail.co.uk>
2012-07-26 11:30:51 +0100

    Added event model objects.

--- Duchess/DUEvent.h
+++ Duchess/DUEvent.h
@@ -8,9 +8,67 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum eventScopeEnum
+{
+    PUBLIC,
+    UNIVERSITY,
+    COLLEGE,
+    SOCIETY
+} EventScope;
 
-@interface DUEvent : NSObject {
+@interface DUEvent : NSObject
+{
+    // Instance variables
     
+    NSString   *_name;
+    NSString   *_descriptionHeader;
+    NSString   *_descriptionBody;
+    NSDate     *_startDate;
+    NSDate     *_endDate;
+    NSString   *_contactTelephoneNumber;
+    NSString   *_contactEmailAddress;
+    NSString   *_accessibilityInformation;
+    NSString   *_linkedWebsiteURL;
+    NSString   *_imageURL;
+    NSString   *_adImageURL;
+    NSString   *_iCalURL;
+    NSString   *_address1;
+    NSString   *_address2;
+    NSString   *_city;
+    NSString   *_postcode;
+    NSString   *_latitude;
+    NSString   *_longitude;
+    NSInteger   _numberOfReviews;
+    NSInteger   _averageReview;
+    NSString   *_associatedCollege;
+    NSString   *_associatedSociety;
+    EventScope  _eventScope;
+    NSArray    *_categories;
 }
+
+@property (nonatomic,retain) NSString   *name;
+@property (nonatomic,retain) NSString   *descriptionHeader;
+@property (nonatomic,retain) NSString   *descriptionBody;
+@property (nonatomic,retain) NSDate     *startDate;
+@property (nonatomic,retain) NSDate     *endDate;
+@property (nonatomic,retain) NSString   *contactTelephoneNumber;
+@property (nonatomic,retain) NSString   *contactEmailAddress;
+@property (nonatomic,retain) NSString   *accessibilityInformation;
+@property (nonatomic,retain) NSString   *linkedWebsiteURL;
+@property (nonatomic,retain) NSString   *imageURL;
+@property (nonatomic,retain) NSString   *adImageURL;
+@property (nonatomic,retain) NSString   *iCalURL;
+@property (nonatomic,retain) NSString   *address1;
+@property (nonatomic,retain) NSString   *address2;
+@property (nonatomic,retain) NSString   *city;
+@property (nonatomic,retain) NSString   *postcode;
+@property (nonatomic,retain) NSString   *latitude;
+@property (nonatomic,retain) NSString   *longitude;
+@property (nonatomic)        NSInteger   numberOfReviews;
+@property (nonatomic)        NSInteger   averageReview;
+@property (nonatomic,retain) NSString   *associatedCollege;
+@property (nonatomic,retain) NSString   *associatedSociety;
+@property (nonatomic)        EventScope  eventScope;
+@property (nonatomic,retain) NSArray    *categories;
 
 @end
--- Duchess/DUEvent.m
+++ Duchess/DUEvent.m
@@ -11,4 +11,29 @@
 
 @implementation DUEvent
 
+@synthesize name = _name;
+@synthesize descriptionHeader = _descriptionHeader;
+@synthesize descriptionBody = _descriptionBody;
+@synthesize startDate = _startDate;
+@synthesize endDate = _endDate;
+@synthesize contactTelephoneNumber = _contactTelephoneNumber;
+@synthesize contactEmailAddress = _contactEmailAddress;
+@synthesize accessibilityInformation = _accessibilityInformation;
+@synthesize linkedWebsiteURL = _linkedWebsiteURL;
+@synthesize imageURL = _imageURL;
+@synthesize adImageURL = _adImageURL;
+@synthesize iCalURL = _iCalURL;
+@synthesize address1 = _address1;
+@synthesize address2 = _address2;
+@synthesize city = _city;
+@synthesize postcode = _postcode;
+@synthesize latitude = _latitude;
+@synthesize longitude = _longitude;
+@synthesize numberOfReviews = _numberOfReviews;
+@synthesize averageReview = _averageReview;
+@synthesize associatedCollege = _associatedCollege;
+@synthesize associatedSociety = _associatedSociety;
+@synthesize eventScope = _eventScope;
+@synthesize categories = _categories;
+
 @end
