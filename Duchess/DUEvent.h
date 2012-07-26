6529007980e7904628a529fc4526e9f41ad5cd38
Matthew Bates <matthew117@hotmail.co.uk>
2012-07-26 10:58:57 +0100

    Initial commit.

---
+++ Duchess/DUEvent.h
@@ -0,0 +1,16 @@
+//
+//  DUEvent.h
+//  Duchess
+//
+//  Created by Matthew Bates on 26/07/2012.
+//  Copyright 2012 Durham University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DUEvent : NSObject {
+    
+}
+
+@end
---
+++ Duchess/DUEvent.m
@@ -0,0 +1,14 @@
+//
+//  DUEvent.m
+//  Duchess
+//
+//  Created by Matthew Bates on 26/07/2012.
+//  Copyright 2012 Durham University. All rights reserved.
+//
+
+#import "DUEvent.h"
+
+
+@implementation DUEvent
+
+@end
