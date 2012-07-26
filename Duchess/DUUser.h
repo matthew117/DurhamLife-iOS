0a3607aa5e311f082ddb8c02f9b362b7803aa944
Matthew Bates <matthew117@hotmail.co.uk>
2012-07-26 12:26:46 +0100

    Added User objects.

---
+++ Duchess/DUUser.h
@@ -0,0 +1,33 @@
+//
+//  DUUser.h
+//  Duchess
+//
+//  Created by App Dev on 26/07/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface DUUser : NSObject
+{
+    NSInteger  _userID;
+    NSString  *_forename;
+    NSString  *_surname;
+    NSString  *_email;
+    NSString  *_department;
+    NSString  *_college;
+    NSMutableArray *_categoryPreferences;
+    NSMutableArray *_subscribedSocities;
+}
+
+@property (nonatomic)         NSInteger  userID;
+@property (nonatomic, retain) NSString  *forename;
+@property (nonatomic, retain) NSString  *surname;
+@property (nonatomic, retain) NSString  *email;
+@property (nonatomic, retain) NSString  *department;
+@property (nonatomic, retain) NSString  *college;
+@property (nonatomic, retain) NSMutableArray *categoryPreferences;
+@property (nonatomic, retain) NSMutableArray *subscribedSocities;
+
+@end
---
+++ Duchess/DUUser.m
@@ -0,0 +1,23 @@
+//
+//  DUUser.m
+//  Duchess
+//
+//  Created by App Dev on 26/07/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DUUser.h"
+
+
+@implementation DUUser
+
+@synthesize userID = _userID;
+@synthesize forename = _forename;
+@synthesize surname = _surname;
+@synthesize email = _email;
+@synthesize department = _department;
+@synthesize college = _college;
+@synthesize categoryPreferences = _categoryPreferences;
+@synthesize subscribedSocities = _subscribedSocities;
+
+@end
