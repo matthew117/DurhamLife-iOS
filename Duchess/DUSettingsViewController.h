65e5474b1db2953dcc1b494c0b40fb2868e2a295
Matthew Bates <matthew117@hotmail.co.uk>
2012-08-28 18:49:16 +0100

    Committing changes before update.

---
+++ Duchess/DUSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DUSettingsViewController.h
+//  Duchess
+//
+//  Created by Matthew Bates on 28/08/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DUSettingsViewController : UIViewController
+
+@end
---
+++ Duchess/DUSettingsViewController.m
@@ -0,0 +1,44 @@
+//
+//  DUSettingsViewController.m
+//  Duchess
+//
+//  Created by Matthew Bates on 28/08/2012.
+//
+//
+
+#import "DUSettingsViewController.h"
+
+@interface DUSettingsViewController ()
+
+@end
+
+@implementation DUSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
