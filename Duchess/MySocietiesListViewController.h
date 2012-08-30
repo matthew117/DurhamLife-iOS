cd0f2eacae3c6cc179fa2e621f79a8e2a58789c5
Matthew Bates <matthew117@hotmail.co.uk>
2012-08-30 14:25:47 +0100

    Added MySocieties list controller.

---
+++ Duchess/MySocietiesListViewController.h
@@ -0,0 +1,14 @@
+//
+//  MySocietiesListViewController.h
+//  Duchess
+//
+//  Created by Matthew Bates on 30/08/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "DUSocietyListViewController.h"
+
+@interface MySocietiesListViewController : DUSocietyListViewController
+
+@end
