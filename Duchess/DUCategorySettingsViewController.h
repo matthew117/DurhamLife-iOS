12f4ec544e753035ca0b1a6a51a2e0efe2487978
Matthew Bates <matthew117@hotmail.co.uk>
2012-08-30 10:17:06 +0100

    Added category settings

---
+++ Duchess/DUCategorySettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DUCategorySettingsViewController.h
+//  Duchess
+//
+//  Created by App Dev on 30/08/2012.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DUCategorySettingsViewController : UITableViewController
+
+@end
