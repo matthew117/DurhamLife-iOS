6f06f4bd4812da87e932c773a84b277be06476d7
Matthew Bates <matthew117@hotmail.co.uk>
2012-09-03 15:39:11 +0100

    Added calendar files

---
+++ Duchess/DUCalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  DUCalendarViewController.h
+//  Duchess
+//
+//  Created by App Dev on 03/09/2012.
+//
+//
+
+#import "DUEventListViewController.h"
+
+@interface DUCalendarViewController : DUEventListViewController
+
+@end
---
+++ Duchess/DUCalendarViewController.m
@@ -0,0 +1,44 @@
+//
+//  DUCalendarViewController.m
+//  Duchess
+//
+//  Created by App Dev on 03/09/2012.
+//
+//
+
+#import "DUCalendarViewController.h"
+
+@interface DUCalendarViewController ()
+
+@end
+
+@implementation DUCalendarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
